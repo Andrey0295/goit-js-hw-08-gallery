c171bd070234c30b7e5f3ebced1aa13d72070c62
Andrey0295 <andreevicandrej00@gmail.com>
2020-10-15 14:33:41 +0300

    Выполняет доп.задание

--- js/home-work-8.js
+++ js/home-work-8.js
@@ -1,11 +1,20 @@
 import { default as imageRefs } from './gallery-items.js';
-// console.log(imageRefs);
 
-const getGalleryListEl = document.querySelector('.js-gallery');
-const getLightboxEl = document.querySelector('.lightbox');
+const galleryRefs = {
+  BodyEl: document.querySelector('body'),
+  GalleryListEl: document.querySelector('.js-gallery'),
+  LightboxEl: document.querySelector('.lightbox'),
+  OriginalImgEl: document.querySelector('.lightbox__image'),
+  CloseBtnEl: document.querySelector('button[data-action="close-lightbox"]'),
+  BackdropEl: document.querySelector('.lightbox__overlay'),
+};
+
 const imagesMarkUp = makeItemGallery(imageRefs);
-getGalleryListEl.insertAdjacentHTML('beforeend', imagesMarkUp);
-console.log(getGalleryListEl);
+galleryRefs.GalleryListEl.insertAdjacentHTML('beforeend', imagesMarkUp);
+
+galleryRefs.BodyEl.addEventListener('click', onOpenLightBox);
+galleryRefs.CloseBtnEl.addEventListener('click', onCloseLightBox);
+galleryRefs.BackdropEl.addEventListener('click', onBackdropClick);
 
 function makeItemGallery(imageRefs) {
   return imageRefs
@@ -19,19 +28,42 @@ function makeItemGallery(imageRefs) {
     .join('');
 }
 
-getGalleryListEl.addEventListener('click', onImageClick);
-function onImageClick(evt) {
+console.log(makeItemGallery);
+
+function onOpenLightBox(evt) {
   evt.preventDefault();
   if (evt.target.nodeName !== 'IMG') {
     return 'не попал!!!';
   }
-  getLightboxEl.classList.add('is-open');
-  const getOriginalImgEl = document.querySelector('.lightbox__image');
-  getOriginalImgEl.setAttribute('src', evt.target.dataset.source);
-  getOriginalImgEl.setAttribute('alt', evt.target.alt);
-  console.log(evt.target.dataset.source);
-  console.log('Клацнули в картинку');
-  console.log(evt.target.alt);
+
+  galleryRefs.LightboxEl.classList.add('is-open');
+  galleryRefs.OriginalImgEl.setAttribute('src', evt.target.dataset.source);
+  galleryRefs.OriginalImgEl.setAttribute('alt', evt.target.alt);
+  galleryRefs.BodyEl.addEventListener('keydown', onPressEscapeKey);
 }
 
-// console.log(getOriginalImgEl);
+function onCloseLightBox(evt) {
+  // evt.preventDefault();
+  galleryRefs.LightboxEl.classList.toggle('is-open');
+  galleryRefs.OriginalImgEl.removeAttribute('src');
+  galleryRefs.OriginalImgEl.removeAttribute('alt');
+  galleryRefs.BodyEl.removeEventListener('keydown', onPressEscapeKey);
+}
+
+function onPressEscapeKey(evt) {
+  // const ESC_KEY_CODE = 'Escape';
+  // const isEscKey = evt.code === ESC_KEY_CODE;
+
+  const isEscKey = evt.key === 'Escape';
+
+  if (isEscKey) {
+    onCloseLightBox();
+  }
+}
+
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    console.log('Click on Backdrop!!!!Uraaa');
+    onCloseLightBox();
+  }
+}
